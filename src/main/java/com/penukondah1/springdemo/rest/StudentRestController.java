761df1da1cb87c0b1fb895ace8d9d9698cae6e68
harika penukonda <penukondaharika@gmail.com>
2020-09-25 12:11:29 -0500

    PathVariable

--- src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
+++ src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
@@ -3,9 +3,12 @@ package com.penukondah1.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.penukondah1.springdemo.entity.Student;
 
@@ -13,20 +16,31 @@ import com.penukondah1.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-//	@GetMapping("/students")
-//	public String getStudents() {
-//		return "List of students";
-//		
-//	}
+	private List<Student> theStudents;
 	
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	// define @PostConstruct to load the student data...
+	
+	@PostConstruct
+	/* The PostConstruct annotation is used on a method that needs to be executed after dependency injection is done to perform any initialization.
+	 * This method MUST be invoked before the class is put into service. */
+	public void loadData() {
 		
-		List<Student> theStudents = new ArrayList<>();
+	    theStudents = new ArrayList<>();
+		
 		theStudents.add(new Student("Alice","Cooper"));
 		theStudents.add(new Student("Betty","Cooper"));
 		theStudents.add(new Student("Jughead","Jones"));
 		theStudents.add(new Student("Archie","Andrews"));
+		
+	}
+	
+
+	// defining end point for "/students" - return list of students
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
+		
 		return theStudents;
 		
 	}
