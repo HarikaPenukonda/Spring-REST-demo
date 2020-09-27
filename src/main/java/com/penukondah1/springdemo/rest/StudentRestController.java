53862a4e99f1b7ae6cc6e3382aec113e4d6b8521
harika penukonda <penukondaharika@gmail.com>
2020-09-27 12:23:20 -0500

    Update StudentRestController.java

--- src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
+++ src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
@@ -60,35 +60,7 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 	
-	// Add an exception handler using @ExceptionHandler
-	@ExceptionHandler // This method is an exception handler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) // StudentErrorResponse --> Type of the response body, StudentNotFoundException --> Exception type to handle/catch
-	{
-		
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return Response
-		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-	}
 	
-	// add another exception handler... to catch any exception (catch all)
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc)
-	{
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return Response
-		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-		
-	}
 }
 
 
