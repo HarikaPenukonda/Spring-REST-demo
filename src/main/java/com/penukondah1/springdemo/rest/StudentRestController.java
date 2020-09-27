d1677fc358509f47c5d7c3b9e660ad972bf0cc82
harika penukonda <penukondaharika@gmail.com>
2020-09-27 11:59:45 -0500

    Adding an exception handler

--- src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
+++ src/main/java/com/penukondah1/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +23,6 @@ public class StudentRestController {
 	private List<Student> theStudents;
 	
 	// define @PostConstruct to load the student data...
-	
 	@PostConstruct
 	/* The PostConstruct annotation is used on a method that needs to be executed after dependency injection is done to perform any initialization.
 	 * This method MUST be invoked before the class is put into service. */
@@ -37,7 +39,6 @@ public class StudentRestController {
 	
 
 	// defining end point for "/students" - return list of students
-	
 	@GetMapping("/students")
 	public List<Student> getStudents(){
 		
@@ -49,8 +50,50 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
 		// just index into the list... keep it simple for now
-		return theStudents.get(studentId);
 		
+		// check the studentId against list size
+		
+		if((studentId >= theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student ID not found - " +studentId);
+		}
+		
+		return theStudents.get(studentId);
+	}
+
+
+	
+	
+	
+	// Add an exception handler using @ExceptionHandler
+	@ExceptionHandler // This method is an exception handler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) // StudentErrorResponse --> Type of the response body, StudentNotFoundException --> Exception type to handle/catch
+	{
+		
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return Response
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
